2d9ef2840648392ba9460f886d1fc98cddfe68b5
taichunmin <taichunmin@gmail.com>
2020-12-25 03:03:40 +0800

    20201225

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
     _: 'readonly',
     axios: 'readonly',
     CryptoJS: 'readonly',
+    dayjs: 'readonly',
     joi: 'readonly',
     JSON5: 'readonly',
     liff: 'readonly',
@@ -30,6 +31,7 @@ module.exports = {
   },
   rules: {
     'eol-last': ['error', 'never'],
+    'multiline-ternary': 0, // 0 = off, 1 = warn, 2 = error
     'no-return-await': 0, // 0 = off, 1 = warn, 2 = error
     'spaced-comment': ['error', 'always', { markers: ['-'] }],
     'comma-dangle': ['error', {
