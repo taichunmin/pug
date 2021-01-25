011dc2e72785fcfab3092a59760d06ce8ae40679
taichunmin <taichunmin@gmail.com>
2021-01-26 03:18:17 +0800

    20210126

--- build.js
+++ build.js
@@ -1,24 +1,47 @@
 const _ = require('lodash')
-const fsPromises = require('fs').promises
 const fg = require('fast-glob')
+const fsPromises = require('fs').promises
+const htmlMinifier = require('html-minifier').minify
 const log = require('debug')('app:index')
 const path = require('path')
 const pug = require('pug')
+const UglifyJS = require('uglify-es')
 
 exports.build = async () => {
   const PUG_OPTIONS = {
     basedir: path.resolve(__dirname, 'src'),
   }
 
+  const htmlMinifierOptions = {
+    caseSensitive: true,
+    collapseBooleanAttributes: true,
+    collapseInlineTagWhitespace: true,
+    collapseWhitespace: true,
+    conservativeCollapse: true,
+    decodeEntities: true,
+    minifyCSS: true,
+    minifyJS: code => UglifyJS.minify(code).code,
+    removeCDATASectionsFromCDATA: true,
+    removeComments: true,
+    removeCommentsFromCDATA: true,
+    removeEmptyAttributes: true,
+    removeRedundantAttributes: true,
+    removeScriptTypeAttributes: true,
+    removeStyleLinkTypeAttributes: true,
+    sortAttributes: true,
+    sortClassName: true,
+    useShortDoctype: true,
+  }
+
   // compile pug files
   const pugFiles = _.map(_.filter(await fg('src/**/*.pug'), file => {
-    if (/\/layouts\/[^/]+\.pug$/.test(file)) return false
+    if (/\/(layout|compoment)-[^/]+\.pug$/.test(file)) return false
     return true
   }), file => file.slice(4))
 
   for (const file of pugFiles) {
     try {
-      const html = pug.renderFile(path.resolve(__dirname, 'src', file), PUG_OPTIONS)
+      const html = htmlMinifier(pug.renderFile(path.resolve(__dirname, 'src', file), PUG_OPTIONS), htmlMinifierOptions)
       const dist = path.resolve(__dirname, 'dist', file.replace(/\.pug$/, '.html'))
       await fsPromises.mkdir(path.dirname(dist), { recursive: true })
       await fsPromises.writeFile(dist, html)
